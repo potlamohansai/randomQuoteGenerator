7763bdb9844d1099b34edab22262e4e8b4aa8db5
Mohan Sai Potla <potlamohansai@gmail.com>
2022-10-15 20:58:58 +0530

    second commit

---
+++ src/components/quoteBox.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const QuoteBox = () => {
+    return (
+        <div id="quote-box">
+            <div id="text">
+
+
+            </div>
+            <div id="author">
+
+            </div>
+            <button id="new-quote">New Quote</button>
+            <button id="tweet-quote"><a target="_blank" href="twitter.com/intent/tweet">Twitter</a></button>
+        </div>
+        
+    )
+}
+
+export default QuoteBox;
